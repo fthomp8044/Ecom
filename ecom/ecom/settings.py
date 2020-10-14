900229be063b197fe31a3b977e1f15ce2294443f
fthomp8044 <fthomp2430@gmail.com>
2020-10-14 12:13:18 -0400

    able to store images in django

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # copy paste from corsheaders
 CORS_ALLOW_ALL_ORIGINS = True
 
