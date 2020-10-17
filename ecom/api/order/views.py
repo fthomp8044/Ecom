bd70944a5bae3a0d2906521bc901ef1edddff5d6
fthomp8044 <fthomp2430@gmail.com>
2020-10-16 21:26:42 -0400

    adding order in the admin

--- ecom/api/order/views.py
+++ ecom/api/order/views.py
@@ -18,3 +18,31 @@ def validate_user_session(id, token):
         return False
     except UserModel.DoesNotExist:
         raise
+
+@csrf_exempt
+def add(request, id, token):
+    if not validate_user_session(id, token):
+        return JsonResponse({'error': 'Please re-login', 'code':'500'})
+
+    if request.method == "POST":
+        user_id = id
+        transaction_id = request.POST['transaction_id']
+        amount = request.POST['amount']
+        products = request.POST['products']
+
+        total_pro = len(products.split(',')[:-1])
+
+        UserModel = get_user_model()
+
+        try:
+            user = UserModel.objects.get(pk=user_id)
+        except UserModel.DoesNotExist:
+            return JsonResponse({'error': 'User does not exists'})
+
+        ordr = Order(user=user, product_names=products, total_products=total_pro, transaction_id=transaction_id, total_amount=amount )
+        ordr.save()
+        return JsonResponse('success': True, 'error': False, 'msg':'Order Placed Succesfully')
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all().order_by('id')
+    serializer_class = OrderSerializer
