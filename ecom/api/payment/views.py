b280cac807ea8be5a47f52e1e426586c5370d7a3
fthomp8044 <fthomp2430@gmail.com>
2020-10-20 20:35:46 -0400

    process the payment from the backend

--- ecom/api/payment/views.py
+++ ecom/api/payment/views.py
@@ -26,7 +26,7 @@ def validate_user_session(id, token):
             return True
         return False
     except UserModel.DoesNotExist:
-        return Falss
+        return False
 
 @csrf_exempt
 def generate_token(request, id, token):
@@ -34,3 +34,26 @@ def generate_token(request, id, token):
         return JsonResponse({'error': 'Invalid Session, Please login again!'})
 
     return JsonResponse({'ClientToken': gateway.client_token.generate(), 'success': True})
+
+@csrf_exempt
+def process_payment(request, id, token):
+    if not validate_user_session(id, token):
+        return JsonResponse({'error': 'Invalid Session, Please login again!'})
+
+    nonce_from_the_client = request.POST["paymentMethodNonce"]
+    amount_from_the_client = request.POST["amount"]
+
+    results = gateway_transaction.sale({
+        "amount": amount_from_the_client,
+        "payment_method_nonce": nonce_from_the_client,
+        "options": {
+            "submit_for_settlement": True
+        }
+    })
+
+    if result.is_success:
+        return JsonResponse({
+            'success': result.is_success,
+            'transaction': {'id': result.transaction.id, 'amount': result.transaction.amount}})
+    else:
+        return JsonResponse({'error': True, 'success': False})
