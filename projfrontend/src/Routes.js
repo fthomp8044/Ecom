ad074d74c83f29cf6ee348570b7f00d3c71b60b7
fthomp8044 <fthomp2430@gmail.com>
2020-11-13 14:01:15 -0500

    setup routes for signin & got the from ready for signin

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 import Home from "./core/Home";
 import Signup from "./user/Signup";
+import Signin from "./user/Signin";
 import PrivateRoutes from "./auth/helper/PrivateRoutes";
 import UserDashboard from "./user/UserDashboard";
 
@@ -13,6 +14,7 @@ const Routes = () => {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/signup" exact component={Signup} />
+        <Route path="/signin" exact component={Signin} />
         <PrivateRoutes path="/user/dashboard" exact component={UserDashboard} />
       </Switch>
     </Router>
---
+++ projfrontend/src/user/Signin.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import Base from '../core/Base';
+import { Link } from 'react-router-dom';
+
+const Signin = () => {
+  const [values, setValues] = useState({
+    name:"",
+    email:"",
+    password:"",
+    error:"",
+    success: false,
+    loading: false,
+    didRedirect: false
+  })
+  // destructer so i dont have to say values.name
+  const { name, email, password, error, success, loading, didRedirect } = values;
+ // higher order function
+  const handleChange = (name) =>
+   (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value })
+  };
+  const successMessage = () => {
+    return(
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div className="alert alert-success"
+          style={{ display: success ? "" : "none" }}>
+            New account succesfully. Please <Link
+            to="/signin"> login now
+            </Link>
+          </div>
+        </div>
+      </div>
+
+    )
+  }
+
+  const errorMessage = () => {
+    return(
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div className="alert alert-danger"
+          style={{ display: error ? "" : "none" }}>
+            Check all fields again
+          </div>
+        </div>
+
+      </div>
+
+    )
+  }
+
+  const signInForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input className="form-control" value={email} onChange={handleChange("email")} type="text"/>
+            </div>
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input className="form-control" value={password} onChange={handleChange("password")} type="password"/>
+            </div>
+            <button onClick={() => {}} className="btn btn-success btn-block">Submit</button>
+          </form>
+        </div>
+      </div>
+    )
+  };
+
+  return(
+    <Base title="Welcome to Signin Page" description="843 Clothing Store">
+      {signInForm()}
+      <p className="text-center">
+        {JSON.stringify(values)}
+      </p>
+    </Base>
+  )
+}
+
+export default Signin;
